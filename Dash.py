08b2b24501b6c93e9625b9a6e2e36069ba69f914
Carlos Avila <caaavilaga@unal.edu.co>
2022-06-18 12:44:16 -0500

    Rename file

--- Dash.py
+++ Dash.py
@@ -5,4 +5,27 @@ Editor de Spyder
 Este es un archivo temporal.
 """
 
-print("Hello World!)
+# Abrir la base de datos
+#!pip install pyreadstat
+#!pip install pandas
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#import pyreadstat
+
+
+dtafile = "C:/Users/carlo/OneDrive - Universidad de los Andes/Barómetro de las Américas/2020/Muestra Nacional 2020/Dataset/App Web/Datos 2004 - 2018.csv"
+df = pd.read_csv(dtafile,sep = "|")
+
+#df.wt.value_counts()
+df2 = df[['year','soct2']]
+
+
+df['year'] = df['year'].astype('int')
+
+cross_tab_prop = pd.crosstab(index = df['year'],columns = df['soct2'], normalize="index")
+
+cross_tab_prop.plot(kind='bar', 
+                    stacked=True, 
+                    colormap='tab10', 
+                    figsize=(10, 6))
